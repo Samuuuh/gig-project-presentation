08af4ae51d2461eb03fd4e4454bc850d9507e587
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2022-03-29 18:12:54 +0100

    web server running on docker

--- web/app/gig.py
+++ web/app/gig.py
@@ -1,25 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 from flask import request
 from flask import jsonify
 from flask import send_from_directory
 
-app = Flask(__name__,
-            static_folder='templates',
-            template_folder='templates')
+app = Flask(__name__)
 
-#@app.route("/")
-#def index():
-#    return render_template('index.html')
-    
 @app.route('/', defaults=dict(filename=None))
 @app.route('/<path:filename>', methods=['GET', 'POST'])
-def index(filename):
+def get_file(filename):
     filename = filename or 'index.html'
-    
+
+    if filename == 'game-of-thrones':
+        filename = 'projects/game-of-thrones/index.html'
+
     if request.method == 'GET':
         return send_from_directory('.', filename)
 
     return jsonify(request.data)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0')
